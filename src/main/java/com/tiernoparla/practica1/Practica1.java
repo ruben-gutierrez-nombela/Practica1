2049955fb5715c0248ae488b9534a25457967dac
Ruben Gutierrez <ruben.gutierrez@srvdealerbest.com>
2022-11-22 13:33:45 +0100

    pruebas para la parte 2

--- src/main/java/com/tiernoparla/practica1/Practica1.java
+++ src/main/java/com/tiernoparla/practica1/Practica1.java
@@ -5,14 +5,14 @@ import java.lang.Math;
 import java.util.Arrays;
 
 public class Practica1 {
-   public static void main(String[] args) {
-       
-    // creación del mensaje
+   static void Mensaje(){
+    // CREACIÓN DEL MENSAJE
         // código
-    int N = 7;
+    int N = 2;
     int M = 0;
     int mensaje2 [] = new int [N*N];
     int mensaje [][] = new int [N][N];
+    System.out.println("El mensaje a enviar es:");
        
     for (int x = 0; x < mensaje.length; x++) {
         for (int i = 0; i < mensaje[x].length; i++) {
@@ -23,25 +23,31 @@ public class Practica1 {
             M++;
         }// for
     }// for
+   }// Method Mensaje
+   
+   public static void main(String[] args) {
+   
+   // Ejecución del método de la parte 1 el Mensaje    
+   Mensaje(); 
     
     
-    // acciones del Sender
+    // ACCIONES DEL SENDER
         // código
     
         
         
         
         
-    // acciones del Noise
+    // ACCIONES DEL NOISE
         // código
         
-    // acciones del Reciever
+    // ACCIONES DEL RECIEVER
         // código
        
-    // método 1 que calcula tal
+    // MÉTODO 1 QUE CALCULA TAL
         // código
         
-    // método 2 que hace cual    
+    // MÉTODO 2 QUE CALCULA CUAL    
         // código
         
    }// main 
