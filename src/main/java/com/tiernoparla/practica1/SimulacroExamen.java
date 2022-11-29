e857715bbf6d6ac12b140d934921941a0728054a
Ruben Gutierrez <ruben.gutierrez@srvdealerbest.com>
2022-11-29 09:56:59 +0100

    sin refactorizar

--- src/main/java/com/tiernoparla/practica1/SimulacroExamen.java
+++ src/main/java/com/tiernoparla/practica1/SimulacroExamen.java
@@ -4,7 +4,7 @@ package com.tiernoparla.practica1;
 import java.util.Scanner;
 
 public class SimulacroExamen {
-          public static void main(String[] args) {
+    public static void main(String[] args) {
            
         Scanner sc = new Scanner(System.in);
 
