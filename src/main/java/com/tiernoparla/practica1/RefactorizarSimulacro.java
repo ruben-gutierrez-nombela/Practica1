70ade1e049b55bd3d75c54451e3012bd93dd0fe3
Ruben Gutierrez <ruben.gutierrez@srvdealerbest.com>
2022-11-29 09:57:25 +0100

    Simulacro refactorizado

---
+++ src/main/java/com/tiernoparla/practica1/RefactorizarSimulacro.java
@@ -0,0 +1,109 @@
+
+package com.tiernoparla.practica1;
+
+import java.util.Scanner;
+
+public class RefactorizarSimulacro {
+    public static void main(String[] args) {
+           
+        Scanner sc = new Scanner(System.in);
+
+        int N = 8;
+        int tab[] = new int[N];
+
+        int Position = 0;
+        int Position_machine = (int) (Math.random() * N);
+        int Player = 7;
+        int Machine = 2;
+        boolean ganador = true;
+
+        juegoTresEnRaya(tab, N, Position, sc, Player, Position_machine, 
+                        Machine, ganador);
+    }// main
+
+    public static void juegoTresEnRaya(int[] tab, int N, int Position, 
+                      Scanner sc, int Player, int Position_machine, int Machine, 
+                      boolean ganador) {
+        for (int i = 0; i < tab.length; i++) {
+            jugadaDePlayer(N, Position, sc, tab, Player);
+
+            System.out.println("");
+            System.out.println("--------------------------");
+            System.out.println("");
+
+            jugadaDeMachine(Position_machine, N, tab, Machine);
+
+            ganador = verQuienGana(tab, ganador);
+            
+            if (ganador == false) {
+                break;
+            }// if
+        }// for
+    }// juegoTresEnRaya
+
+    public static boolean verQuienGana(int[] tab, boolean ganador) {
+        for (int j = 1; j < tab.length - 1; j++) {
+            if (tab[j] == 7) {
+                if (tab[j] - tab[j - 1] == tab[j + 1] - tab[j]) {
+                    System.out.println("Ha ganado el jugador");
+                    ganador = false;
+                    break;
+                }// if
+            }// if
+            if (tab[j] == 2) {
+                if (tab[j] - tab[j - 1] == tab[j + 1] - tab[j]) {
+                    System.out.println("Ha ganado el ordenador");
+                    ganador = false;
+                    break;
+                }// if
+            }// if
+        }// for
+        return ganador;
+    }// verQuienGana
+
+    public static void jugadaDeMachine(int Position_machine, int N, int[] tab, 
+                                       int Machine) {
+        for (int j = 0; j < 9999; j++) {
+            
+            Position_machine = (int) (Math.random() * N);
+            
+            while (tab[Position_machine] == 2 ||
+                   tab[Position_machine] == 7) {
+                
+                Position_machine = (int)(Math.random() * N);               
+            }// while
+        }// for
+        
+        System.out.println("El ordenador ha colocado su número "
+                + "en la posicion " + Position_machine);
+        
+        tab[Position_machine] = Machine;
+        System.out.println("");
+        
+        for (int j = 0; j < tab.length; j++) {
+            System.out.print(tab[j]);
+        }// for
+        
+        System.out.println("");
+        System.out.println("El ordenador ha terminado su turno");
+    }// jugadaDeMachine
+
+    public static void jugadaDePlayer(int N, int Position, Scanner sc, 
+                                      int[] tab, int Player) {
+        
+        System.out.println("Dame una posicion del 0 al " + (N-1) + ": ");
+        Position = sc.nextInt();
+        
+        while (tab[Position] == 2 || tab[Position] == 7) {
+            System.out.println("Posicion usada, pon otra posicion");
+            Position = sc.nextInt();
+        }// while
+        
+        tab[Position] = Player;
+        System.out.println("");
+        
+        for (int j = 0; j < tab.length; j++) {
+            System.out.print(tab[j]);
+        }// for
+    }// jugadaDePlayer
+}// RefactorizarSimulacro
